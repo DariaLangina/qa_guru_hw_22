9a0126d9050694c9e2c7b3c706b8e2dac2ada965
DariaLangina <dlangina@rvision.ru>
2022-02-06 10:59:25 +0300

    fix test

--- src/test/java/dlangina/tests/selenoid/WikipediaTests.java
+++ src/test/java/dlangina/tests/selenoid/WikipediaTests.java
@@ -1,9 +1,11 @@
 package dlangina.tests.selenoid;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.back;
 import static io.qameta.allure.Allure.step;
 
@@ -18,31 +20,16 @@ public class WikipediaTests extends SelenoidTestBase {
   @AllureId("7181")
   @Test
   @Tag("Selenoid")
-  @DisplayName("Добавление Русского языка")
-  void settingLanguageTest() {
+  @DisplayName("Successful search in wikipedia android app")
+  void searchTest() {
     back();
 
-    step("Open menu", () ->
-             $(MobileBy.id("org.wikipedia.alpha:id/menu_icon")).click()
-        );
-    step("Open settings", () ->
-             $(MobileBy.id("org.wikipedia.alpha:id/main_drawer_settings_container")).click()
-        );
-    step("Open 'Wikipedia languages' section", () ->
-             $(MobileBy.xpath(("//*[@text='Wikipedia languages']"))).click()
-        );
-    step("Click 'Add language' button", () ->
-             $(MobileBy.xpath(("//*[@text='ADD LANGUAGE']"))).click()
-        );
-    step("Search and select Russian language", () -> {
-           $(MobileBy.id("org.wikipedia.alpha:id/menu_search_language")).click();
-           $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("russian");
-           $(MobileBy.id("org.wikipedia.alpha:id/localized_language_name")).shouldHave(text("Русский")).click();
-         }
-        );
-    back();
-    step("Check 'Wikipedia languages' has russian language", () ->
-             $(MobileBy.xpath(("//*[@text='English, Русский']"))).shouldBe(visible)
-        );
+    step("Type search", () -> {
+      $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+      $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).val("BrowserStack");
+    });
+    step("Verify content found", () ->
+        $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+            .shouldHave(sizeGreaterThan(0)));
   }
 }
